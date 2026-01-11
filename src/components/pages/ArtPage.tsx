02546da57aab2add834816aefa2419d1674588b2
Wix Remote Machine <support@wix.com>
2026-01-11 13:53:33 +0000

    Manual code changes

--- src/components/pages/ArtPage.tsx
+++ src/components/pages/ArtPage.tsx
@@ -285,7 +285,7 @@ export default function ArtPage() {
   ].map((src, i) => (
     <div
       key={i}
-      className="relative break-inside-avoid overflow-hidden shadow-lg hover:shadow-2xl transition-all duration-300"
+      className="relative overflow-hidden shadow-lg hover:shadow-2xl transition-all duration-300"
     >
       <Image
         src={src}
