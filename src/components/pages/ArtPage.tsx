ca2ad1466b3830c772e1b10bca5bedf40b2d07a5
Wix Remote Machine <support@wix.com>
2026-01-11 13:54:40 +0000

    Manual code changes

--- src/components/pages/ArtPage.tsx
+++ src/components/pages/ArtPage.tsx
@@ -273,7 +273,7 @@ export default function ArtPage() {
             Our Collection
           </h2>
           
-          <div className="columns-1 sm:columns-2 lg:columns-3">
+          <div className="columns-1 sm:columns-2 lg:columns-3 bg-red-500">
   {/* Masonry Item */}
   {[
     "https://static.wixstatic.com/media/e86273_ef52a94d65e04eb5baba36492d97c2c4~mv2.png?originWidth=576&originHeight=512",
