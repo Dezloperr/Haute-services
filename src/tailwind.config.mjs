cd76d87aaed2aa9c9c1c725539a0acdd8508a942
Wix Remote Machine <support@wix.com>
2026-01-24 17:49:49 +0000

    Manual code changes

--- src/tailwind.config.mjs
+++ src/tailwind.config.mjs
@@ -26,7 +26,7 @@ export default {
                 'gold-accent': '#E3E3E3',
                 destructive: '#D32F2F',
                 'destructive-foreground': '#FFFFFF',
-                background: '#fef8f8',
+                background: '#fefafa',
                 secondary: '#000000',
                 foreground: '#000000',
                 'secondary-foreground': '#FFFFFF',
